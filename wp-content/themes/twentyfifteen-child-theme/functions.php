d2008af456e30da11d2fec205dca412374e86609
Lino Espinoza <lino.espinoza@entel.pe>
2017-11-09 18:21:38 -0500

    Avances de la sesion 6

--- wp-content/themes/twentyfifteen-child-theme/functions.php
+++ wp-content/themes/twentyfifteen-child-theme/functions.php
@@ -1,16 +1,11 @@
 <?php
-//
-// Recommended way to include parent theme styles.
-//  (Please see http://codex.wordpress.org/Child_Themes#How_to_Create_a_Child_Theme)
-//  
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array('parent-style')
-    );
+function enqueue_styles() {
+	// Embebe el archivo style.css del tema actual (en nuestro caso por estar usando child themes nos devuelve la url del tema padre)
+	wp_enqueue_style('style_css', get_template_directory_uri() . '/style.css');
+	
+	// Embebe el archivo main.css ubicado en la carpeta /css del tema activo (al estar utilizando child themes, nos devuelve la url del tema hijo
+	wp_enqueue_style('main_css', get_stylesheet_directory_uri() . '/css/main.css');
 }
-//
-// Your code goes below
-//
+add_action('wp_enqueue_scripts', 'enqueue_styles');
+
+?>
