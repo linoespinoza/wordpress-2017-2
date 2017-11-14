ed27efdf6174a7776eeadb19499cfbc806f5ab82
Lino Espinoza <lino.espinoza@entel.pe>
2017-11-14 18:24:26 -0500

    Fix 2

---
+++ wp-content/themes/twentyfifteen-child-theme/_index.php
@@ -0,0 +1,15 @@
+<?php 
+get_header();
+?>
+<?php 
+echo get_template_directory_uri() . "<br />"; 
+echo get_stylesheet_directory_uri();
+?>
+<div id="container">
+	<h1>Página Principal</h1>
+	<p>lorem ipsum</p>
+	<!-- Contenido del sitio -->
+</div>
+<?php
+get_footer();
+?>
--- wp-content/themes/twentyfifteen-child-theme/functions.php
+++ wp-content/themes/twentyfifteen-child-theme/functions.php
@@ -1,11 +1,21 @@
 <?php
+/**
+ ** Funcion que embebe archivos css a nuestro tema
+ **/
 function enqueue_styles() {
-	// Embebe el archivo style.css del tema actual (en nuestro caso por estar usando child themes nos devuelve la url del tema padre)
-	wp_enqueue_style('style_css', get_template_directory_uri() . '/style.css');
-	
-	// Embebe el archivo main.css ubicado en la carpeta /css del tema activo (al estar utilizando child themes, nos devuelve la url del tema hijo
-	wp_enqueue_style('main_css', get_stylesheet_directory_uri() . '/css/main.css');
+
+	wp_enqueue_style('bootstrap_min_css', "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css");
+	wp_enqueue_style('carousel_css', get_template_directory_uri() . '/css/carousel.css');
+
 }
 add_action('wp_enqueue_scripts', 'enqueue_styles');
+
+/**
+ ** Funcion que embebe archivos js a nuestro tema
+ **/
+function enqueue_js() {
+	//wp_enqueue_script();
+}
+add_action('wp_enqueue_scripts', 'enqueue_js');
 
 ?>
